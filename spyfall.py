19651acd853ab64a5d84f48a25a0227fc2471b1d
Colton Holoday <choloday@umich.edu>
2018-01-17 17:14:16 -0500

    Impelement cli

--- spyfall.py
+++ spyfall.py
@@ -1,17 +1,30 @@
 import smtplib
+import click
 
-with open('config', 'r') as config_file:
-    src_email = config_file.readline().rstrip()
-    src_password = config_file.readline().rstrip()
+@click.command()
+@click.option('-c', '--config', type=str,
+        help="Specifies the source email's config file. DEFAULT=config")
+@click.argument('emails_filename', type=str, nargs=1)
+@click.argument('session_name', type=str, nargs=1)
+def main(emails_filename, session_name, config):
+    if config is None:
+        config = "config"
 
-with open('players', 'r') as emails_file:
-    player_emails = [email.rstrip() for email in emails_file.readlines()]
+    with open(config, 'r') as config_file:
+        src_email = config_file.readline().rstrip()
+        src_password = config_file.readline().rstrip()
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(src_email, src_password)
+    with open(emails_filename, 'r') as emails_file:
+        player_emails = [email.rstrip() for email in emails_file.readlines()]
 
-msg = "Hello World"
-for email in player_emails:
-    server.sendmail(src_email, email, msg)
-server.quit()
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(src_email, src_password)
+
+    msg = "Hello World"
+    for email in player_emails:
+        server.sendmail(src_email, email, msg)
+    server.quit()
+
+if __name__ == "__main__":
+    main()
